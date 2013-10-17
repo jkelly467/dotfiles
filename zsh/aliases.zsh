0071f331e68cd65fe3500a12a336984a4c081008
Jon Kelly <jkelly@adc4gis.com>
2013-10-16 20:36:52 -0500

    update; zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,4 +18,23 @@ alias gpot='git push origin test'
 alias gca='git commit -a'
 alias gst='git status'
 
-alias rwin='rdesktop -g 1280x1024 -P -z -x l -r sound:off -u webadmin 172.25.8.55 &'
+alias rwin='rdesktop -g 1280x1024 -P -z -x l -r sound:off -u adzmar 172.25.8.244 &' #adzmar1
+alias rwinIE='rdesktop -g 1280x1024 -P -z -x l -r sound:off -u webadmin 172.25.3.147 &'
+
+alias -g "backlight"="/bin/bash /usr/bin/asus-screen-brightness"
+alias -g "klight"="asus-kbd-backlight"
+
+alias ud='sudo packer -Syu --noedit'
+
+alias vpn='sudo vpnc default'
+
+alias selt='sh /home/jkelly/scripts/selt.sh'
+alias eltdep='sh /home/jkelly/scripts/eltdep.sh'
+alias breakdown='sh /home/jkelly/scripts/breakdown.sh'
+alias mongocast='mongo ds031407.mongolab.com:31407/zer0cast -u zer0cast -p retroafrica'
+alias stagdep='ssh deployment@deployment "cd /home/deployment/extra/extra-utils/auto_deploy; /usr/bin/python easy_single_click_deploy.py -p test -f staging.csv"'
+alias releasedep='sh /home/jkelly/scripts/releasedep.sh'
+alias redisserv='cd /var/log/redis; redis-server'
+alias rake='noglob rake'
+
+alias pbar='aoss pianobar'
